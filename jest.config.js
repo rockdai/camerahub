6a14e0946c037a26e4c6e4fbc8064e0830a08860
RockBot <rockbot@macmini-2014.local>
2026-02-13 16:18:50 +0800

    test: add istanbul coverage reporting

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 /** @type {import('jest').Config} */
 module.exports = {
   testEnvironment: 'node',
-  clearMocks: true
+  clearMocks: true,
+  collectCoverageFrom: ['index.js'],
+  coverageDirectory: 'coverage'
 };
